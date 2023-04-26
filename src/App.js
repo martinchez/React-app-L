e0463342cfc654deaed0840c2ec7e9a4147b9a4b
martinchez <martinchege1998@gmail.com>
2023-04-26 11:02:29 +0300

    readme update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AgroGIS <code>Powering </code>Smart Agriculture with Satellite data.
         </p>
         <a
           className="App-link"
