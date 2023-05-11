f3013197f70f493a9f3d89fecd47fb718a545788
martinchez <martinchege1998@gmail.com>
2023-05-11 18:33:29 +0300

    children prop

--- src/index.js
+++ src/index.js
@@ -1,33 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './css/index.css'
-// creating a component
+
+// const author = 'Martin Chege'
+// const title = 'Interesting Facts For Curious Minds'
+// const img = './images/book.jpg'
+
 function BookList() {
   return (
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   )
 }
 
-const Book = () => {
+// const Book = ({author, title, img, children}) => {
+// }
+const Book = (props) => {
+  console.log(props)
+  const {author, title, img } = props
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2> {title}</h2>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   )
 }
+const firstBook = {
+  author: 'Jordan Moore',
+  title: 'Interesting Facts For Curious Minds',
+  img: './images/book.jpg',
+}
+const secondBook = {
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+}
 
-const Image = () => (
-  <img src="./images/book.jpg" />
-)
-const Title = () => <h2> There Are Moms Way Worse Than You</h2>
-const Author = () => <h4>Author</h4>
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
