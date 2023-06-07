c73f31b6ceb550a70ba27127e4e180931d66ff02
martinchez <martinchege1998@gmail.com>
2023-06-07 09:47:46 +0300

    complex modules to get individua ID

--- src/index.js
+++ src/index.js
@@ -2,47 +2,47 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './css/index.css'
 
-
 const books = [
   {
     author: 'Jordan Moore',
     title: 'Interesting Facts For Curious Minds',
     img: './images/book.jpg',
-    id:1,
+    id: 1,
   },
   {
     author: 'James Clear',
     title: 'Atomic Habits',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-    id:2,
+    id: 2,
   },
-];
+]
 
 function BookList() {
-  const someValue ="shake hands"
-  const displayValue = () => {
-    console.log(someValue)
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id)
+    console.log(book)
   }
   return (
     <section className="bookList">
-      {books.map((book) =>{
-        const { img, title, author ,id} = book
-        return <Book {...book} key={book.id} displayValue ={displayValue} />
+      {books.map((book) => {
+        const { img, title, author, id } = book
+        return <Book {...book} key={book.id} getBook={getBook} />
       })}
-      {/* <someComponent someValue = {someValue} /> */}
     </section>
   )
 }
 
 const Book = (props) => {
-  // console.log(props)
-  const { img, title, author,displayValue } = props
-  
+  const { img, title, author, id, getBook } = props
+  const getSingleBook = () => {
+    getBook(id)
+  }
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2> {title}</h2>
-      <button onClick={displayValue}>Click Title </button>
+      {/* <button onClick={getSingleBook}>Click Title </button> */}
+      <button onClick={getSingleBook}>Click Title </button>
       <h4>{author}</h4>
     </article>
   )
